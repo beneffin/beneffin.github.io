683d88786fd4122de407a35a75eeea5a35fd6595
Dian Rachmawati <diansoraya@MacBook-Air-Aya.local>
2016-09-20 00:20:26 -0400

    update

--- Homework/GiphyAPICalculator/javascript.js
+++ Homework/GiphyAPICalculator/javascript.js
@@ -9,17 +9,17 @@ var query = "rainbow"
 //Trending Here ya
 
 
-function setup() {
-  noCanvas();
-  var url = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
-  loadJSON(url, gotData);
-}
+// function setup() {
+//   noCanvas();
+//   var url = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+//   loadJSON(url, gotData);
+// }
 
-function gotData(giphy) {
-  for (var i = 0; i < giphy.data.length; i++) {
-    createImg(giphy.data[i].images.original.url);
-  }
-}
+// function gotData(giphy) {
+//   for (var i = 0; i < giphy.data.length; i++) {
+//     createImg(giphy.data[i].images.original.url);
+//   }
+// }
 
 //Search Function Here ya
 
